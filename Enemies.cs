5c725b12b415949774db647de39c9f0bbff34c30
Jason062100 <57514035+Jason062100@users.noreply.github.com>
2024-07-18 15:20:52 -0500

    Create Enemies.cs

---
+++ Enemies.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jason;
+
+namespace Jason
+{
+    public class Enemies
+    {
+        static Random random = new Random();
+
+        //Enemy Attack
+        public int EnemyAttack(int playerHealth, int p, string n)
+        {
+            int enemyAttack = random.Next(0, p + 1);
+
+            int newPlayerHealth = playerHealth - enemyAttack;
+
+            Console.WriteLine($"The {n} hit you back for {enemyAttack} damage leaving you on {newPlayerHealth} health.");
+
+            return newPlayerHealth;
+        }
+    }
+}
