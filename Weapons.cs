5455d3c52e413ccdc612983445c82ea034dca782
Jason062100 <57514035+Jason062100@users.noreply.github.com>
2024-07-22 14:43:19 -0500

    Update Weapons.cs

--- Weapons.cs
+++ Weapons.cs
@@ -8,6 +8,23 @@ namespace Jason
 {
     public class Weapons
     {
-        
+        Random random = new Random();
+        Player player = new Player();
+
+        public int HealingStaff(int userH)
+        {
+            int health = userH;
+
+            int healChance = random.Next(0, 5); // 1/5 chance to heal
+            if (healChance == 0) 
+            {
+                health += 5;
+                Console.WriteLine($"Your Healing Staff healed you for 5 health. Your health is now: {health}.");
+            }
+
+            return health;
+        }
+
+
     }
 }
