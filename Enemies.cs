05ed7de82cfae7b377895325842f7ead056a459f
Jason062100 <57514035+Jason062100@users.noreply.github.com>
2024-07-23 15:15:32 -0500

    Update Enemies.cs

--- Enemies.cs
+++ Enemies.cs
@@ -17,38 +17,32 @@ namespace Jason
             int armor;
             int enemyAttack = random.Next(0, p + 1);
 
-            switch (Program.currentPlayer.Class)
+            switch (Program.currentPlayer.Class) //Checks class, uses armor for that class, and negates damage based on armor
             {
                 case "Warrior":
                     armor = random.Next(0, Program.currentPlayer.highArmor + 1);
                     enemyAttack -= armor;
-                    Console.WriteLine($"{armor} damage was negated by your armor.");
+                    Console.WriteLine($"{armor} damage from the enemy is negated by your armor.");
                     break;
                 
                 case "Mage":
                     armor = random.Next(0, Program.currentPlayer.lowArmor + 1);
                     enemyAttack -= armor;
-                    Console.WriteLine($"{armor} damage was negated by your armor.");
+                    Console.WriteLine($"{armor} damage from the enemy is negated by your armor.");
                     break;
 
                 case "Rogue":
                     armor = random.Next(0, Program.currentPlayer.lowArmor + 1);
                     enemyAttack -= armor;
-                    Console.WriteLine($"{armor} damage was negated by your armor.");
+                    Console.WriteLine($"{armor} damage from the enemy is negated by your armor.");
                     break;
             }
 
-            if (enemyAttack < 0)
-            {
-                enemyAttack = 0;
-            }
+            if (enemyAttack < 0) enemyAttack = 0; //If enemy is dead, don't attack
             
             int newPlayerHealth = playerHealth - enemyAttack;
 
-            if (newPlayerHealth < 0)
-            {
-                newPlayerHealth = 0;
-            }
+            if (newPlayerHealth < 0) newPlayerHealth = 0; //If player health drops below 0, set to 0
 
             Console.WriteLine($"The {n} hit you back for {enemyAttack} damage leaving you on {newPlayerHealth} health.");
 
