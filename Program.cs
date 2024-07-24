76819241cf951f1e742a4c8b0e5060f32a615a59
Jason062100 <57514035+Jason062100@users.noreply.github.com>
2024-07-24 14:48:15 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,7 +9,6 @@ namespace Jason
 {
     public class Program
     {
-        public static Player currentPlayer = new Player();
         public static Shop shop = new Shop();
 
         public static string playAgain = "y";
@@ -18,21 +17,18 @@ namespace Jason
         {
             while (playAgain == "y")
             {
-                shop.UseShop();
+                Player player = new Player(100, 1, 0); //100 health, level 1, starting xp
 
-                //Starts game and resets player stats
+                //Starts game
                 Console.Title = "RPG Game";
-                Encounters.ResetPlayer();
-                Start();
+                Start(player);
 
 
-                //Encounters
-
+                //Dungeon 1
                 //Encounter 1
-                if (Encounters.userH > 0) Encounters.Combat("Zombie", 15, 30); //(Name, Power, Health)
-                Encounters.ResetStatus();
+                Encounters.Combat(player, "Zombie", 15, 30); //(Name, Power, Health)
 
-                if (Encounters.userH > 0)
+                if (player.Health > 0)
                 {
                     Console.WriteLine("You stumble upon a hatch. Maybe this could be the way out...");
                     Console.WriteLine("Press any key to continue.");
@@ -41,10 +37,9 @@ namespace Jason
                 }
 
                 //Encounter 2
-                if (Encounters.userH > 0) Encounters.Combat("Goblin", 20, 40);
-                Encounters.ResetStatus();
+                if (player.Health > 0) Encounters.Combat(player, "Goblin", 20, 40);
 
-                if (Encounters.userH > 0)
+                if (player.Health > 0)
                 {
                     Console.WriteLine("There is a lever on the wall. You decide to pull it and see a huge door that you thought was just a wall open.");
                     Console.WriteLine("Press any key to continue.");
@@ -53,17 +48,34 @@ namespace Jason
                 }
 
                 //Encounter 3
-                if (Encounters.userH > 0) Encounters.Combat("Skeleton Knight", 25, 60);
-                Encounters.ResetStatus();
+                if (player.Health > 0) Encounters.Combat(player, "Skeleton Knight", 30, 60);
 
+                if (player.Health > 0)
+                {
+                    Console.WriteLine("Press any key to continue.");
+                    Console.ReadKey();
+                    Console.Clear();
+                    Console.WriteLine("After defeating the boss, you find the way out of the dungeon!");
+                    Console.WriteLine("Finally you see daylight again. Your eyes take a while to adjust.");
+                    Console.WriteLine("Press any key to continue.");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+                //Exit dungeon
+                shop.UseShop(player);
+                //Encounters.ResetForDungeon();
+
+                //Dungeon 2   Troll, Golem, Griffin
+                Encounters.Combat(player, "Troll", 25, 60);          //TBD
 
 
                 //Win
-                if (Encounters.userH > 0) Console.WriteLine("You win. Congrats!");
+                if (player.Health > 0) Console.WriteLine("You win. Congrats!");
 
 
                 //Lose
-                if (Encounters.userH <= 0) Console.WriteLine($"You have been slain. Your adventure is over. GG.");
+                if (player.Health <= 0) Console.WriteLine($"You have been slain. Your adventure is over. GG.");
 
                 //Play again
                 Console.WriteLine("Would you like to play again? (y/n):");
@@ -71,7 +83,7 @@ namespace Jason
             }
         }
 
-        static void Start()
+        static void Start(Player player)
         {
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Red;
@@ -89,26 +101,27 @@ namespace Jason
                     case 1:
                         Console.Clear();
                         Console.WriteLine("You have chosen a Warrior.");
-                        currentPlayer.Class = "Warrior";
+                        player.ChooseClass("Warrior");
                         x = false;
                         break;
 
                     case 2:
                         Console.Clear();
                         Console.WriteLine("You have chosen a Mage.");
-                        currentPlayer.Class = "Mage";
+                        player.ChooseClass("Mage");
                         x = false;
                         break;
 
                     case 3:
                         Console.Clear();
                         Console.WriteLine("You have chosen a Rogue.");
-                        currentPlayer.Class = "Rogue";
+                        player.ChooseClass("Rogue");
                         x = false;
                         break;
 
                     default:
                         Console.WriteLine("Invalid input. Try again:");
+                        input = Convert.ToInt32(Console.ReadLine());
                         break;
                 }
             }
